75b8bb0dfa3e6e3c33dc37c8de6ca55adea4d5c0
dvega212 <dvega212>
2014-10-07 18:21:06 -0400

    Fixing Traffic Light

---
+++ Photo Gallery/js/application.js
@@ -0,0 +1,14 @@
+ var images = ["images/thumb-0.jpg", "images/thumb-1.jpg", "images/thumb-2.jpg", "images/thumb-3.jpg"]
+ var currentImage = 0
+ function changePicture() {
+ 	alert("YoMTV!")
+
+ }
+
+ function nextImage() {
+ 	
+ }
+
+ function prevImage() {
+ 	
+ }
--- Stop Light/js/application.js
+++ Stop Light/js/application.js
@@ -1,1 +1,15 @@
-function 
+function go(light) {
+
+	var yellowLight = document.getElementById('yellow-light');
+	var greenLight = document.getElementById('green-light');
+	var redLight = document.getElementById('red-light');
+
+	yellowLight.style.backgroundColor = "black";
+	redLight.style.backgroundColor = "black";
+	greenLight.style.backgroundColor = "black";
+
+	var lightId = light + '-light';
+	var chosenLight = document.getElementById(lightId);
+	chosenLight.style.backgroundColor = light; 
+
+}
